f262c29a1baaf54badf0fdc5b41ea41f9fac5d67
Benoit Dunand-Laisin <benoit.dunand-laisin@prologism.fr>
2013-07-12 15:52:23 +0200

    Add Sonar machine

---
+++ environnements/sonar.pp
@@ -0,0 +1,9 @@
+# Sonar Machine
+node 'ip-172-31-42-43.eu-west-1.compute.internal' {
+  apt::source { 'puppetlabs':
+    location   => 'http://apt.puppetlabs.com',
+    repos      => ['main', 'dependencies'],
+    key        => '4BD6EC30',
+    key_server => 'pgp.mit.edu',
+  }
+}
