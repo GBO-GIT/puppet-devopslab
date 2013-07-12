19cb9e1c3548ecd8199270a80dbe23b21b6d0e79
Benoit Dunand-Laisin <benoit.dunand-laisin@prologism.fr>
2013-07-12 17:09:58 +0200

    Add sonar source list

--- environnements/sonar.pp
+++ environnements/sonar.pp
@@ -6,4 +6,18 @@ node 'ip-172-31-42-43.eu-west-1.compute.internal' {
     key        => '4BD6EC30',
     key_server => 'pgp.mit.edu',
   }
+  apt::source { 'sonar':
+    location    => ' http://downloads.sourceforge.net/project/sonar-pkg/deb',
+    release     => 'binary/',
+    repos       => '',
+    include_src => false,
+  }
+  package { 'sonar':
+    ensure       => present,
+    require      => Apt::source['sonar'],
+  }
+  package { 'sonar':
+    ensure       => running,
+    require      => Package['sonar'],
+  }
 }
